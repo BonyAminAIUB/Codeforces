e3eaf8c42e2b5ee08e9470bd68fea4df91e549c2
Bony Amin <bonyamincse@gmail.com>
2024-12-27 01:06:27 +0600

    Elephant

---
+++ 617A.cpp
@@ -0,0 +1,14 @@
+//617A Elephant 
+
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    cin >> x; //take input from user
+
+    int steps = (x + 4) / 5;
+
+    cout << steps << endl; //Output show the lowest steps
+
+    return 0;
+}
