b48d0e612965e98d55dd2c81306edc15a40bbb03
Bony Amin <24-56311-1@student.aiub.edu>
2025-09-09 10:28:26 +0600

    Add files via upload

---
+++ A_Collatz_Conjecture.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int k, x;
+        cin >> k >> x;
+        for(int i = 0; i<k; i++){
+            if((x - 1) % 3 == 0){
+                int y = (x - 1) / 3;
+                if(y % 2 == 1){
+                    x = y;
+                    continue;
+                }
+            }
+            x = x * 2;
+        }
+        cout << x << endl;
+    }
+    return 0;
+}
