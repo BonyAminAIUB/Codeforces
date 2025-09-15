33a193b5f0cd1814ca3c902dc47eafeee4012d7b
Md Bony Amin <24-56311-1@student.aiub.edu>
2025-09-15 18:59:35 +0600

    Add files via upload

---
+++ A_Beautiful_Sequence.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        bool flag = false;
+        for(int i = 0; i<n; i++){
+            cin >> v[i];
+        }
+        for(int i = 0; i<n; i++){
+            if(v[i] <= i+1){
+                flag = true;
+                break;
+            }
+        }
+        if(flag == true){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
